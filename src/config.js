24f22dd1cba129ab7e36b1493c580b34377847fe
Frank Robles <roblesfc@gmail.com>
2019-10-10 15:28:26 -0700

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+STRIPE_KEY: "pk_test_B3Tdf7IYKTMtrx7tCultcEai00nW0IkZqa",
+    MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-uploads-fraaaank"
@@ -12,5 +14,7 @@ export default {
     USER_POOL_ID: "us-east-1_hrEu4kNnx",
     APP_CLIENT_ID: "3t3ioge7n7j2trfjl1u6jlv50k",
     IDENTITY_POOL_ID: "us-east-1:12c60393-36ce-4c8e-97d9-3c843a7cd72d"
-  }
+  },
+
+
 };
