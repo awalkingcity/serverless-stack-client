0f3cb593944fe561b4105918234b54a10523131f
Frank Robles <roblesfc@gmail.com>
2019-10-10 13:07:08 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-fraaaank"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://0q9qyd5fr9.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_hrEu4kNnx",
+    APP_CLIENT_ID: "3t3ioge7n7j2trfjl1u6jlv50k",
+    IDENTITY_POOL_ID: "us-east-1:12c60393-36ce-4c8e-97d9-3c843a7cd72d"
+  }
+};
